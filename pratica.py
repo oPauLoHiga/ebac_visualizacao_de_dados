4ad184a2e541fde780105d3885563f254e5e4182
Paulo Higa <paulojhiga@gmail.com>
2025-11-17 20:39:05 -0300

    Update pratica.py

--- pratica.py
+++ pratica.py
@@ -1,4 +1,14 @@
 import pandas as pd
 
+# Leia o arquivo ‘ecommerce_estatistica.csv’ dentro de um dataframe .
+# Faça uma análise detalhada dos dados, descubra quais dados gostaria de destacar e crie os seguintes gráficos:
+# Gráfico de Histograma
+# Gráfico de dispersão
+# Mapa de calor
+# Gráfico de barra
+# Gráfico de pizza
+# Gráfico de densidade
+# Gráfico de Regressão
+# Adicione títulos nos gráficos e nos eixos para ficar claro os objetivos dos gráficos
 
 df = pd.read_csv('ecommerce_preparados.csv')
