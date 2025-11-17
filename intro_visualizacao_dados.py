5692b4c307181669bab5a6a28f30c7b0084f9695
Paulo Higa <paulojhiga@gmail.com>
2025-11-17 15:32:54 -0300

    Update intro_visualizacao_dados.py

--- intro_visualizacao_dados.py
+++ intro_visualizacao_dados.py
@@ -4,3 +4,41 @@ import pandas as pd
 
 df = pd.read_csv('clientes-v3-preparado.csv')
 print(df.head().to_string())
+
+# Histograma
+plt.hist(df['salario'])
+plt.show()
+
+# Histograma - Parâmetros
+plt.figure(figsize=[10,6])
+plt.hist(df['salario'], bins=100, color='green', alpha=0.8)
+plt.title('Histograma - Distribuição de Salários')
+plt.xlabel('Salário')
+plt.xticks(ticks=range(0, int(df['salario'].max())+2000, 2000))
+plt.ylabel('Frequência')
+plt.grid(True)
+plt.show()
+
+#Múltiplos Gráficos
+plt.figure(figsize=[10,6])
+plt.subplot(2,2,1) # 2 Linhas, 2 Colunas, 1º Gráfico
+#Gráfico de Dispersão
+plt.scatter(df['salario'], df['salario'])
+plt.title('dispersão - Salario e Salario')
+plt.xlabel('salario')
+plt.ylabel('salario')
+
+plt.subplot(1,2,2)
+plt.scatter(df['salario'], df['anos_experiencia'], color='#5883a8', alpha=0.6, s=30) # cor hexadecimal online / 's' tamanho da bola
+plt.title('Dispersão - Salario e Anos de Experiencia')
+plt.xlabel('Salario')
+plt.ylabel('ano de experiencia')
+
+# Mapa de Calor
+corr = df[['salario', 'anos_experiencia']].corr()
+plt.subplot(2,2,3)
+sns.heatmap(corr, annot=True, cmap='coolwarm')
+plt.title('Correlação Salario e Idade')
+
+plt.tight_layout() #ajustar espaçamentos
+plt.show()
